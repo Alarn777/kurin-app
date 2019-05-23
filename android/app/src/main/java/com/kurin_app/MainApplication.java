3899e4e260f5a09c0c1358fee17f77fea1864b8a
michael rokitko <alarn777@gmail.com>
2019-05-23 21:22:57 +0300

    Customer ready                  Socket IO working

--- android/app/src/main/java/com/kurin_app/MainApplication.java
+++ android/app/src/main/java/com/kurin_app/MainApplication.java
@@ -3,6 +3,7 @@ package com.kurin_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage(),
             new RNScreensPackage(),
